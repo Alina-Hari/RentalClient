e48661380747d7f770bdba8d991c9fcf21f68b48
Alina Havriushenko <alinagvr@gmail.com>
2024-03-13 09:15:27 +0100

    style home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
+import { LuAmpersand } from "react-icons/lu";
 
 function Navbar() {
     const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
     return (
-        <div className="navbar w-full h-[10%] bg-base-200">
+        <div className="navbar w-full h-[10%] bg-base-200 md:px-10">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
@@ -26,8 +27,9 @@ function Navbar() {
 
 
             <div className="navbar-center">
-                <Link className="btn btn-ghost text-xl" to="/">
-                    <button>Rent App</button>
+                <Link className="btn btn-ghost text-xl md:text-3xl" to="/">
+                    <div className="flex items-center"><span>Click</span><span className="rotate-180"><LuAmpersand /></span>
+                        <span>Rent</span></div>
                 </Link>
             </div>
             <div className="navbar-end justify-end">
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,11 @@
-import houseImg from '../assets/retro.jpg'
+import netherlands from '../assets/netherlands.png'
+import map from '../assets/map.png'
+import { BiHomeHeart } from "react-icons/bi";
+import { IconContext } from "react-icons";
+import { FaCalendarAlt } from "react-icons/fa";
+import { HiCursorClick } from "react-icons/hi";
+
+
 export default function HomePage() {
 
     const offers = [
@@ -20,37 +27,84 @@ export default function HomePage() {
         }
     ]
     return (
-            <div className="w-full h-full flex flex-col overflow-y-scroll lg:overflow-y-hidden md:flex-row justify-start md:justify-between  rounded-xl  shadow-sm ">
-                <div className="flex h-[100%] flex-col justify-evenly md:w-1/2 p-2 ">
-                    <h2 className='font-bold text-xl md:text-3xl mt-2 mb-2  text-center'>Discover Your Perfect Rental, Embrace Every Moment!</h2>
-                    <p className='mt-2 mb-2 font-bold text-xl text-center'>Let's get acquainted!</p>
-                    <p className='mt-2 mb-2'>Find the perfect place to call home. Whether you're searching for a cozy studio,
-                        a spacious family apartment, or a modern urban loft, we've got you covered.</p>
-                    <div className="flex flex-row justify-evenly w-full h-auto mt-4 mb-4">
-                        <p className="flex flex-col "><span className='text-xl font-bold'>115k+</span><span className="text-xs font-thin">Capital Raised</span></p>
-                        <p className="flex flex-col "><span className='text-xl font-bold'>20k+</span><span className="text-xs font-thin">Happy Customers</span></p>
-                        <p className="flex flex-col "><span className='text-xl font-bold'>40k+</span><span className="text-xs font-thin">Properties Options</span></p>
+        <div className="w-full h-full flex flex-col overflow-y-scroll lg:overflow-y-hidden md:flex-row justify-start md:justify-between  rounded-xl  shadow-sm ">
+            <div className="flex h-[100%] flex-col justify-evenly md:w-1/2 p-2 ">
+                <h2 className='font-bold text-xl md:text-3xl mt-2 mb-2  text-left'>Discover your dream home in the Netherlands - schedule visits with expert agents today!</h2>
+                <p className='mt-2 mb-2'>Find the perfect place to call home. Whether you're searching for a cozy studio,
+                    a spacious family apartment, or a modern urban loft, we've got you covered.</p>
+                {/* <div className="flex flex-row justify-evenly w-full h-auto mt-4 mb-4">
+                    <p className="flex flex-col "><span className='text-xl font-bold'>115k+</span><span className="text-xs font-thin">Capital Raised</span></p>
+                    <p className="flex flex-col "><span className='text-xl font-bold'>20k+</span><span className="text-xs font-thin">Happy Customers</span></p>
+                    <p className="flex flex-col "><span className='text-xl font-bold'>40k+</span><span className="text-xs font-thin">Properties Options</span></p>
+                </div>
+                 */}
+
+
+                <div className="stats shadow rounded-2xl h-32 md:h-24 md:w-fit">
+
+                    <div className="stat p-4">
+                        <div className="stat-figure text-primary">
+                            <FaCalendarAlt className="inline-block w-8 h-6 stroke-current" />
+                        </div>
+                        <div className="text-2xl md:text-4xl font-bold">Book</div>
+                        <div className="stat-title">on one click</div>
                     </div>
-                    <div className="w-[100%] pl-5 pr-5 h-auto carousel rounded-box ">
-                        {offers.map((offer, index) => {
-                            return <div key={index} className="carousel-item w-full ">
-                                <div className="container w-full text-black p-8 mr-3 lg:mr-0 lg:ml-24 bg-[#87563b]  border-black rounded-lg shadow-lg max-w-md mx-auto">
-                                    <div className="text-3xl font-bold mb-4">{offer.name}</div>
+
+                    <div className="stat">
+                        <div className="stat-figure text-primary">
+                            <HiCursorClick className="inline-block w-8 h-8 stroke-current" />
+                        </div>
+                        <div className="text-2xl md:text-4xl font-bold">List</div>
+                        <div className="stat-title">your appartment</div>
+                    </div>
+
+
+                    <div className="stat">
+                        <div className="stat-figure text-secondary">
+                            <div className="avatar flex justify-center items-center">
+                                <div className="w-14 bg-accent mask mask-hexagon flex justify-center items-center">
+                                    <span className="text-white text-center text-3xl flex justify-center align-middle items-center h-full">
+                                        <p className='h-full text-center justify-center align-middle pt-2'>A</p></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="text-2xl md:text-4xl font-bold">Manage</div>
+                        <div className="stat-title">your bookings</div>
+                    </div>
+
+                </div>
+
+                {/* <div className="carousel carousel-center max-w-md p-4 space-x-4 bg-neutral rounded-box">
+                    <div className="carousel-item">
+                        <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="rounded-box" />
+                    </div>
+                </div> */}
+
+                <div className="carousel carousel-center max-w-md p-8 space-x-4 w-full h-60 overflow-y-hidden">
+                    {offers.map((offer, index) => {
+                        return <div key={index} className="carousel-item w-full ">
+                            <div className="card w-[400px] shadow-xl">
+                                <div className="bg-white rounded-xl card-body">
+                                    <h2 className="card-title">{offer.name}</h2>
                                     <div className="text-sm mt-4">
                                         <p>{offer.text}</p>
                                     </div>
+
                                 </div>
                             </div>
-                        })}
+                        </div>
+                    })}
 
-                        {/* <div className="carousel-item w-full">
+                    {/* <div className="carousel-item w-full">
                             <img src="https://daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.jpg" className="w-full" alt="Tailwind CSS Carousel component" />
                         </div> */}
-                    </div>
-                </div>
-                <div className="flex h-[100%] flex-col justify-evenly  md:w-1/2 p-2 ">
-                    <img src={houseImg} className="object-scale-down  max-w-[100%] h-auto" />
                 </div>
             </div>
+            <div className="hidden sm:block relative md:flex h-[100%] flex-col justify-evenly md:w-1/2 p-2 ">
+                <img src={netherlands} className="object-scale-down max-w-[80%] h-auto" />
+                <div className='absolute left-80 top-60 text-5xl'><IconContext.Provider value={{ color: "white" }}>
+                    <BiHomeHeart /></IconContext.Provider></div>
+            </div>
+        </div >
     )
 }
