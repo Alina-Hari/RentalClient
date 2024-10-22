fbb8049b96f50e99ebf5354fe0b1ba8eb7091745
Pravin S <you@example.com>
2024-10-22 23:01:33 +0200

    env added to git ignore

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ function Navbar() {
             </div>
             <div className="navbar-center">
                 <div className="dropdown sm:hidden" >
-                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle" onClick={() => { setToggleVar(!toggleVar) }}>
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle" onClick={() => { setToggleVar(!toggleHamburg) }}>
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
                     </div>
                     {toggleVar && (<ul tabIndex={0} className="menu menu-sm dropdown-content mt-2 z-[1] p-4 shadow-xl bg-base-100 rounded-box w-40">
